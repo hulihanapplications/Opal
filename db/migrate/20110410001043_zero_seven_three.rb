efa9b9b0a802116473cd32e2563a320a456e8e1f
Dave Hulihan <dave@hulihanapplications.com>
2011-06-28 19:33:46 -0600

    Fixed Item Notification Bug

--- db/migrate/20110410001043_zero_seven_three.rb
+++ db/migrate/20110410001043_zero_seven_three.rb
@@ -5,7 +5,7 @@ class ZeroSevenThree < ActiveRecord::Migration
     
     # Create Video Plugin
     plugin = Plugin.create(:name => "Video", :order_number => Plugin.next_order_number, :is_enabled => "1", :is_builtin => "1")
-        PluginSetting.create(:plugin_id => plugin.id, :name => "video_display_mode",   :value => "Full", :options => "Full, List",  :item_type => "option")        
+      PluginSetting.create(:plugin_id => plugin.id, :name => "video_display_mode",   :value => "Full", :options => "Full, List",  :item_type => "option")        
   end
 
   def self.down

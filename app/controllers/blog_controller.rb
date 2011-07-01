d2411931718726ce7c370eb22467a3faf61ff70b
Dave Hulihan <dave@hulihanapplications.com>
2011-07-01 11:25:59 -0600

    Blog#archive now uses Time.mktime instead of Time.strptime(only available in Ruby 1.9.x

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -21,10 +21,8 @@ class BlogController < ApplicationController
   end
 
   def archive
-   # Compute Time based on parameters, should we look to beginning of day, month, or year? 
-   time_string = params[:year].blank? ? "" : params[:year] + (params[:month].blank? ? "" : "-" + params[:month] + (params[:day].blank? ? "" : "-" + params[:day]))
-   format_string = params[:month].blank? ? "" : "%Y" + (params[:month].blank? ? "" : "-%m" + (params[:day].blank? ? "" : "-%d"))  
-   @date = Time.strptime(time_string, format_string)
+   @date = Time.mktime(params[:year], params[:month], params[:day]) 
+   # Compute Time Range based on parameters, should we look to beginning of day, month, or year? 
    range_start = !params[:day].blank? ? @date.beginning_of_day : (!params[:month].blank? ? @date.beginning_of_month : !params[:year].blank? ? @date.beginning_of_year : 10.years.ago.beginning_of_year) 
    range_end =  !params[:day].blank? ? @date.end_of_day : (!params[:month].blank? ? @date.end_of_month : !params[:year].blank? ? @date.end_of_year : Time.now)    
    @pages = Page.all.blog.published.newest_first.paginate :page => params[:page], :per_page => 5, :conditions => ["created_at > ? and created_at < ?", range_start, range_end]

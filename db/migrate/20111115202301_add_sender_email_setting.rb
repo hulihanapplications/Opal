041e20ea6e8a65b09a12d53380e60cd66d8a148d
Dave Hulihan <dave@hulihanapplications.com>
2011-11-14 10:55:17 -0700

    Destroy sender_email setting on downward migration

--- db/migrate/20111115202301_add_sender_email_setting.rb
+++ db/migrate/20111115202301_add_sender_email_setting.rb
@@ -1,8 +1,9 @@
 class AddSenderEmailSetting < ActiveRecord::Migration
   def self.up
-	    Setting.create(:name => "sender_email",  :value => "noreply@nowhere.com", :setting_type => "System", :item_type => "string")
+	  Setting.create(:name => "sender_email",  :value => "noreply@nowhere.com", :setting_type => "System", :item_type => "string")
   end 
 
   def self.down
+    Setting.find_by_name("sender_email").destroy
   end
 end

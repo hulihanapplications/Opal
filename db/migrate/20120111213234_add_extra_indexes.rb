f7c4809a45cedf1e626c2eae90ec6dc315941a1f
Hulihan Applications <dave@hulihanapplications.com>
2012-01-11 14:53:00 -0700

    Added extra indexes and updated render_flash

---
+++ db/migrate/20120111213234_add_extra_indexes.rb
@@ -0,0 +1,21 @@
+class AddExtraIndexes < ActiveRecord::Migration
+  def up
+    add_index :users, :username
+    add_index :users, :email
+    add_index :users, :persistence_token
+    add_index :users, :last_request_at   
+    add_index :settings, :name
+    add_index :groups, :name    
+    add_index :plugins, :name               
+  end
+
+  def down
+    remove_index :users, :username
+    remove_index :users, :email
+    remove_index :users, :persistence_token
+    remove_index :users, :last_request_at   
+    remove_index :settings, :name
+    remove_index :groups, :name  
+    remove_index :plugins, :name                       
+  end
+end

c2af90b30b243aecd6194abb64e1039c3a9efef3
Artyom Silivonchik <mr.hant@gmail.com>
2012-03-10 21:40:42 +0300

    Ticket #523: Added migration for resaving objects
    Continue changes on #523.
    All existing objects doesn't have slugs and
    therefore friendly URLs too.
    Added migration is needed for resaving of all
    existing objects which support friendly URLs:
     * Categories
     * Items
     * Users
     * Pages
    
    So users updating to Opal v0.8.2 from previous
    versions will just need to run "rake db:migrate"
    for update of existing objects.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120310160046) do
+ActiveRecord::Schema.define(:version => 20120310182815) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"

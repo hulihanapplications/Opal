a750529e9d578141cac357f7a1d46bce67f3e952
Dave Hulihan <dave@hulihanapplications.com>
2012-04-17 15:51:07 -0600

    Updated mail initializer & setting views

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -6,44 +6,63 @@ class Setting < ActiveRecord::Base
   
   cattr_accessor :global_settings # Setting.global_settings       
 
-  def self.get_setting(name) # get a setting from the database, usually text-based string
-    setting = Setting.find_by_name(name)
-    return setting.to_s
-  rescue # ActiveRecord not found
-    return ""
-  end
+  class << self
+    def get_setting(name) # get a setting from the database, usually text-based string
+      setting = Setting.find_by_name(name)
+      return setting.to_s
+    rescue # ActiveRecord not found
+      return ""
+    end
+    
+    def get_setting_bool(name) # get a setting from the database return true or false depending on "1" or "0"
+      setting = Setting.find_by_name(name)
+  	  setting.to_bool
+    rescue # ActiveRecord not found
+      return false
+    end  
+    
+    def get_global_settings
+      begin
+    	  logger.info "Retrieving global settings."    
+    	  setting_array = Setting.global # get ALL settings    
+    	  setting = setting_array.hash_by(:name, :to_value) # convert array to hash, indexed by name, value by to_value
+    	  setting[:title] = setting[:site_title]
+    	  setting[:description] = setting[:site_description]
+    	  setting[:theme_url] =  "/themes/#{setting[:theme]}" # url for theme directory
+    	  setting[:themes_dir] =  File.join(Rails.root.to_s, "public", "themes") # system path for main themes directory 
+    	  setting[:theme_dir] =  File.join(setting[:themes_dir], setting[:theme]) # system path for current theme directory 
+    	  setting[:default_preview_type] =  setting[:default_preview_type].constantize if setting[:default_preview_type]
+    	  Rails.application.config.action_mailer.default_url_options = { :host => setting[:host] ? setting[:host] : "localhost" } # set actionmailer host 
+    	  # Autoload plugin settings
+    	  Plugin.plugins.each do |name, plugin|
+    	    setting[plugin.plugin_class.name.underscore.to_sym] = PluginSetting.plugin(plugin).all.hash_by(:name, :to_value) 
+    	  end  	  
+      rescue Exception => e
+        logger.error e
+        #logger.error e.backtrace
+        setting = Hash.new
+     	end
+      return setting
+    end
+
+    # change setting
+    def set(name, value)
+      find_by_name(name).update_attribute(:value, value)
+      reload
+    end  
+    
+    # reload settings from database and store/cache in cattr
+    def reload
+      self.global_settings = table_exists? ? Setting.get_global_settings : Hash.new # load global settings into metaclass variable    
+    end
   
-  def self.get_setting_bool(name) # get a setting from the database return true or false depending on "1" or "0"
-    setting = Setting.find_by_name(name)
-	  setting.to_bool
-  rescue # ActiveRecord not found
-    return false
-  end  
-  
-  def self.get_global_settings
-    begin
-  	  logger.info "Retrieving global settings."    
-  	  setting_array = Setting.global # get ALL settings    
-  	  setting = setting_array.hash_by(:name, :to_value) # convert array to hash, indexed by name, value by to_value
-  	  setting[:title] = setting[:site_title]
-  	  setting[:description] = setting[:site_description]
-  	  setting[:theme_url] =  "/themes/#{setting[:theme]}" # url for theme directory
-  	  setting[:themes_dir] =  File.join(Rails.root.to_s, "public", "themes") # system path for main themes directory 
-  	  setting[:theme_dir] =  File.join(setting[:themes_dir], setting[:theme]) # system path for current theme directory 
-  	  setting[:default_preview_type] =  setting[:default_preview_type].constantize if setting[:default_preview_type]
-  	  Rails.application.config.action_mailer.default_url_options = { :host => setting[:host] ? setting[:host] : "localhost" } # set actionmailer host 
-  	  # Autoload plugin settings
-  	  Plugin.plugins.each do |name, plugin|
-  	    setting[plugin.plugin_class.name.underscore.to_sym] = PluginSetting.plugin(plugin).all.hash_by(:name, :to_value) 
-  	  end  	  
-    rescue Exception => e
-      logger.error e
-      #logger.error e.backtrace
-      setting = Hash.new
-   	end
-    return setting
-  end
-  
+    # override [] for easy setting retrieval
+    #   Setting[:title] # => "My Website"
+    def [](setting)
+      global_settings[setting.to_sym]
+    end    
+  end 
+
   def to_param # make custom parameter generator for friendly urls
     "#{id}-#{name.gsub(/[^a-z0-9]+/i, '-')}"
   end
@@ -67,17 +86,6 @@ class Setting < ActiveRecord::Base
   def to_value # return a value based on setting type
   	item_type == "bool" ? to_bool : to_s 
   end 
-  
-  # change setting
-  def self.set(name, value)
-    find_by_name(name).update_attribute(:value, value)
-    reload
-  end  
-  
-  # reload settings from database and store/cache in cattr
-  def self.reload
-    self.global_settings = table_exists? ? Setting.get_global_settings : Hash.new # load global settings into metaclass variable    
-  end
   
   # Load settings for the first time when model is loaded
   reload  
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,18 +1,17 @@
 # Load Mail Settings from config/email.yml
 Rails.application.class.configure do 
-  config.action_mailer.raise_delivery_errors = false # report email errors?...usually overriden by config/environments/[environment]
-    
+  config_file = Rails.root.join("config/email.yml")
+  
   if Rails.env != 'test'
-    if File.exists?("#{Rails.root.to_s}/config/email.yml")
+    if File.exists?(config_file)
       config.action_mailer.delivery_method = :smtp
-      email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/email.yml"))
+      email_settings = YAML::load(File.open(config_file))
       config.action_mailer.smtp_settings = email_settings[Rails.env] unless email_settings[Rails.env].nil?
+      # Load host from config file if specified
+      config.action_mailer.default_url_options ||= {:host => email_settings[Rails.env][:host]} if email_settings[Rails.env][:host]
     else # no mail config file found
       config.action_mailer.delivery_method = :sendmail if File.exists?(Emailer.sendmail_settings[:location]) # attempt to use use sendmail
+      config.action_mailer.default_url_options ||=  {:host => "localhost"} # set actionmailer default host       
     end
   end
-
-  config.action_mailer.default_url_options ||=  {:host => "localhost"} # set actionmailer default host 
 end
-
-

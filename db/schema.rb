4a9b6983f7bcf4c3225cabe65a23c4b383c045c8
Hulihan Applications <dave@hulihanapplications.com>
2012-05-21 17:33:10 -0600

    Created migration: @RemoveSectionSettings@ and updated views.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120311001715) do
+ActiveRecord::Schema.define(:version => 20120521230728) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -139,7 +138,6 @@ ActiveRecord::Schema.define(:version => 20120311001715) do
     t.boolean  "display_children",     :default => true
     t.boolean  "group_access_only",    :default => false
     t.string   "group_ids"
-    t.text     "side"
     t.string   "slug"
   end
 

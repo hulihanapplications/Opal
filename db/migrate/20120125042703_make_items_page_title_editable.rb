cdd3586a9693ef7a227a45609927dcf51e4ad0c1
Hulihan Applications <dave@hulihanapplications.com>
2012-06-02 13:28:11 -0400

    Another migration reversion fix for friendly_id

--- db/migrate/20120125042703_make_items_page_title_editable.rb
+++ db/migrate/20120125042703_make_items_page_title_editable.rb
@@ -1,4 +1,5 @@
 class MakeItemsPageTitleEditable < ActiveRecord::Migration
+  class Page < ActiveRecord::Base; end
   def up
     items_page = Page.find_by_name("items")
     if !items_page.nil?
--- db/migrate/20120310123336_add_slug_to_pages.rb
+++ db/migrate/20120310123336_add_slug_to_pages.rb
@@ -8,6 +8,6 @@ class AddSlugToPages < ActiveRecord::Migration
     remove_column :pages, :slug
     remove_index :pages, :slug if index_exists?(:pages, :slug)
     # Reset columns so FriendlyID won't break other migrations
-  	Page.reset_column_information
+    Page.reset_column_information
   end 
 end
